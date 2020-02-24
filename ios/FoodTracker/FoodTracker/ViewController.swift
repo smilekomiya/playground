f06f2b2f8d03be8c2091d8ba9e0cb9eaeb7f2341
smilekomiya <vetwith@gmail.com>
2020-02-24 17:31:46 +0900

    build a basic ui

---
+++ ios/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by yuzuru komiya on 2020/02/24.
+//  Copyright © 2020 yuzuru komiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
